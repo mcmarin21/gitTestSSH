aac7101a5d62ff6df50e595975dbd1fa08e15c6f
CarritoHG1435 <hhernandezc2202@alumno.ipn.mx>
2024-03-14 20:05:30 -0600

    XD

--- Hola.java
+++ Hola.java
@@ -1,8 +1,9 @@
-piblic class Hola
+public class Hola
 {
 	public static void main(String args[]){
 		System.out.println("hola");
 		System.out.println("hola");
+		System.out.println("Adios Carlitos"); 
 	}
 }
 
